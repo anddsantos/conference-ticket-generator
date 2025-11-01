421aea6114b7cdccfbe1db9d5118500d187ed1e0
4ndr31 <andrei.pinto@escola.pr.gov.br>
2025-11-01 13:53:41 -0300

    added email verification and a animation while generatin ticket

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ changeInput.addEventListener('change', () => {
         const type = changeInput.files[0].type;
         const formats = ['image/jpeg', 'image/png'];
         const fileSize = (changeInput.files[0].size / 1024).toFixed(2);
-        
+
         if (!formats.includes(type) || fileSize > 500) {
             upFileDesc.innerText = 'File too large or wrong file type. Please upload a valid format and size file';
             upFileDesc.classList.add('error');
@@ -71,27 +71,90 @@ changeInput.addEventListener('change', () => {
 
         const img = document.createElement('img');
         img.id = 'user-icon';
-        img.className = 'user-icon'; 
+        img.className = 'user-icon';
         img.src = URL.createObjectURL(changeInput.files[0]);
 
-        dropzone.innerHTML = '';  
+        dropzone.innerHTML = '';
         dropzone.appendChild(img);
 
         afterUpload.style.display = 'flex';
         afterUpload.style.opacity = 1;
 
-        removeImageBtn.addEventListener('click', () => {
+        removeImageBtn.addEventListener('click', (e) => {
+            e.preventDefault()
             img.remove()
         })
     }
 });
 
+//email validation
+const emailInput = document.querySelector('#email')
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const email = emailInput.value
+    const isValid = validatEmail(email)
+
+    if (!isValid) {
+        iziToast.show({
+            title: 'Invalid email',
+            message: 'Please check your email format',
+            position: 'bottomLeft',
+            timeout: 3100,
+            backgroundColor: 'hsl(7, 88%, 67%)'
+        })
+    }
+    else {
+        animation()
+    }
+})
+
+function validatEmail(email) {
+    const regex = /^[^\s]+@[^\s]+\.[^\s]+$/
+    return regex.test(email)
+}
 
 //github user name input
 const githubInput = document.querySelector('#gitHub')
-githubInput.addEventListener('focus' , () => {
-    if(githubInput.value === '') githubInput.value = '@'
-})  
+githubInput.addEventListener('focus', () => {
+    if (githubInput.value === '') githubInput.value = '@'
+})
 
 //generate ticket
-const generateBtn = document.querySelector('#generate-btn')
+const ticket = document.querySelector('.ticket-container')
+
+function animation() {
+    const header = document.querySelector('.header')
+    const loader = document.querySelector('.loader')
+    const loaderStatus = document.querySelector('.loader-status')
+
+    if(loader){
+        loader.style.display = 'block'
+        loader.offsetHeight;
+    }
+    form.style.display = 'none'
+    header.style.display = 'none'
+
+    const status = [
+        'Analyzing information',
+        'Putting everything in its place',
+        'Generating code',
+        'Generating ticket'
+    ]
+
+    let i = 0;
+
+    const interval = setInterval(() => {
+        loaderStatus.textContent = status[i];
+        i++;
+
+        if (i >= status.length) {
+            clearInterval(interval);
+            loader.style.display = 'none'
+            ticket.style.display = 'block'
+            loaderStatus.display = 'none'
+        }
+    }, 2000);
+}
